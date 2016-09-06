be44aa0ba17c439e9ef1542ebebac8d4221d5d38
ukatama <dev.ukatama@gmail.com>
2016-09-06 14:39:45 +0900

    Commit-Push

--- src/dodontof-watcher.js
+++ src/dodontof-watcher.js
@@ -1,6 +1,12 @@
 const https = require('https');
 const config = require('config');
 const Twitter = require('twitter');
+const git = require('nodegit');
+const path = require('path');
+const promisify = require('promisify-node');
+const fse = promisify(require('fs-extra'));
+
+fse.ensureDir = promisify(fse.ensureDir);
 
 function handleError(callback) {
     return function handler(error) {
@@ -14,7 +20,7 @@ function handleError(callback) {
     }
 }
 
-function getToken() {
+function handle() {
     const credential = new Buffer(
         [
             'twitter.consumer_key',
@@ -63,7 +69,67 @@ function getToken() {
                         version: tweet.version[0],
                         url: `http://www.dodontof.com/DodontoF/DodontoF_${tweet.version[0]}.zip`
                     }));
+                
                 console.dir(releases);
+                const release = releases[0];
+
+                git.Clone(config.get('git.cloneURL'), `/tmp/dw-${release.version}`)
+                    .then(repo => {
+                        const versionCache = path.join(repo.workdir(), 'VERSION');
+                        const template = path.join(repo.workdir(), 'Dockerfile.template');
+                        const dockerfile = path.join(repo.workdir(), 'Dockerfile');
+                
+                        return fse.readFile(versionCache, 'utf8')
+                            .catch(() => null)
+                            .then(data => (
+                                data !== release.version ? Promise.resolve() : Promise.reject(new Error('Already Updated'))
+                            ))
+                            .then(() => fse.readFile(template, 'utf8'))
+                            .then(data => data.replace('<DODONTOF_VERSION>', release.version))
+                            .then(data => fse.outputFile(dockerfile, data, 'utf8'))
+                            .then(() => fse.outputFile(versionCache, release.version))
+                            .then(() => repo.refreshIndex())
+                            .then(index =>
+                                Promise.all([
+                                    index.addByPath('Dockerfile'),
+                                    index.addByPath('VERSION'),
+                                ])
+                                    .then(() => index.write())
+                                    .then(() => index.writeTree())
+                                    .then(oid =>
+                                        git.Reference.nameToId(repo, 'HEAD')
+                                            .then(head => repo.getCommit(head))
+                                            .then(parent => {
+                                                const author = git.Signature.create.apply(
+                                                    git.Signature,
+                                                    config.get('git.author')
+                                                );
+                                                const comitter = git.Signature.create.apply(
+                                                    git.Signature,
+                                                    config.get('git.comitter')
+                                                );
+
+                                                return repo.createCommit('HEAD', author, comitter, release.version, oid, [parent]);
+                                            })
+                                    )
+                            )
+                            .then(() => git.Remote.create(repo, 'origin-push', config.get('git.pushURL')))
+                            .then(remote => remote.push(
+                                ['refs/heads/master:refs/heads/master'],
+                                {
+                                    callbacks: {
+                                        credentials: (url, userName) => git.Cred.sshKeyNew(
+                                            userName,
+                                            path.join(__dirname, '../config/id_rsa.pub'),
+                                            path.join(__dirname, '../config/id_rsa'),
+                                            ''),
+                                    },
+                                }
+                            ));
+                    })
+                    .catch(e => {
+                        console.error(e);
+                    });
             }));
         });
     });
@@ -72,6 +138,4 @@ function getToken() {
     req.end();
 }
 
-getToken();
-
-
+module.exports.handle = handle;
